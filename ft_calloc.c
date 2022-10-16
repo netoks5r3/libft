77d1020e4b3c04f79dce6330c5010dd19518da1b
wremelli <wremelli@student.42sp.org.br>
2022-10-17 00:53:21 +0200

    t

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wremelli <nremelli@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/05 05:00:14 by wremelli          #+#    #+#             */
+/*   Updated: 2022/10/16 16:50:17 by wremelli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ret;
+
+	ret = malloc(size * count);
+	if (!ret)
+		return (NULL);
+	ft_bzero(ret, size * count);
+	return (ret);
+}
